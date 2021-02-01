f7bd55d30d7e968989fd6302b1e6b7b7af995dd0
hannah chan <hannah.n.kahnwald@gmail.com>
2021-02-01 15:39:23 -0600

    Tokens added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+c onst http = require('http');
 const https = require('https');
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
@@ -7,8 +7,8 @@ const fs = require('fs');
 const _data = require('./lib/lib');
 const handlers = require('./lib/handlers');
 const helpers = require('./lib/helpers');
-
-const { httpPort, httpsPort, name } = require('./lib/config')
+const { httpPort, httpsPort, name } = require('./lib/config');
+const {isFunction} = require('util');
 
 const httpsServerOptions = {
   key: fs.readFileSync('./https/key.pem'),
@@ -24,13 +24,14 @@ const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
 });
 
 const unifiedServer = (httpsServerOptions, (req, res) => {
+  const { headers } = req;
   const method = req.method.toLowerCase();
-  var parsedUrl = url.parse(req.url, true)
+  const parsedUrl = url.parse(req.url, true)
   const { pathname, query } = parsedUrl;
   const trimmedPath = pathname.replace(/^\/+|\/+$/g,'');
 
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
   req.on('data', data => {
     buffer += decoder.write(data);
@@ -42,6 +43,7 @@ const unifiedServer = (httpsServerOptions, (req, res) => {
     const choosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath]: handlers.notFound;
 
     var data = {
+      'headers': headers,
       'path': pathname,
       'method': method,
       'payload': helpers.parseJSONtoObject(buffer),
@@ -64,6 +66,7 @@ const unifiedServer = (httpsServerOptions, (req, res) => {
 const router = {
   'ping': handlers.ping,
   'users': handlers.users,
+  'tokens': handlers.tokens,
 };
 
 httpServer.listen(httpPort,() => {
